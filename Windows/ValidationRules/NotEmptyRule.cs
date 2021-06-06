59043203f669f11b07143a2673dc162e2598a566
Leonardo-Cosmos <kylin_cl@hotmail.com>
2021-06-06 22:36:47 +0800

    Implement multi command execution

---
+++ Windows/ValidationRules/NotEmptyRule.cs
@@ -0,0 +1,28 @@
+﻿/* 2021/6/6 */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TerminalMonitor.Windows.ValidationRules
+{
+    class NotEmptyRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var currentValue = value as string;
+
+            if (String.IsNullOrEmpty(currentValue))
+            {
+                return new ValidationResult(false, ErrorMessage ?? "Value should not be empty");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+
+        public string ErrorMessage { get; set; }
+    }
+}
