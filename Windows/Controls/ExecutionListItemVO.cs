439923afd41c35f4e64c297b4b7652d4effe0b25
Leonardo-Cosmos <kylin_cl@hotmail.com>
2021-06-10 19:17:39 +0800

    Implement multi command execution

---
+++ Windows/Controls/ExecutionListItemVO.cs
@@ -0,0 +1,23 @@
+﻿/* 2021/6/10 */
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TerminalMonitor.Windows.Controls
+{
+    class ExecutionListItemVO : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set { name = value; OnPropertyChanged(); }
+        }
+    }
+}
---
+++ Windows/Controls/ExecutionListView.xaml.cs
@@ -0,0 +1,51 @@
+﻿/* 2021/6/10 */
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TerminalMonitor.Execution;
+
+namespace TerminalMonitor.Windows.Controls
+{
+    /// <summary>
+    /// Interaction logic for ExecutionListView.xaml
+    /// </summary>
+    public partial class ExecutionListView : UserControl
+    {
+        private readonly ObservableCollection<ExecutionListItemVO> executionVOs = new();
+
+        public ExecutionListView()
+        {
+            InitializeComponent();
+
+            lstExecutions.ItemsSource = executionVOs;
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        public void AddExecution(Execution.Execution execution)
+        {
+
+        }
+
+        public void RemoveExecution(Execution.Execution execution)
+        {
+
+        }
+
+    }
+}
