9f93e07ced0666fc7159c0079a3feef7be2a7385
Leonardo-Cosmos <kylin_cl@hotmail.com>
2021-07-06 14:03:08 +0800

    Fix unhandled exception makes app crash

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿/* 2021/4/16 */
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace TerminalMonitor
 {
@@ -13,5 +16,20 @@ namespace TerminalMonitor
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            // Process unhandled exception
+            Debug.WriteLine($"Unhandled exception: {e.Exception.Message}");
+            Debug.WriteLine(e.Exception.StackTrace);
+            MessageBox.Show("A unhandled exception just occurred.", "Unhandled Exception",
+                MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            // Prevent default unhandled exception processing
+            e.Handled = true;
+        }
     }
 }
